a208374d9fb1e497b8f719d3f72c1f407709fe0b
brennoclemente <80718967+brennoclemente@users.noreply.github.com>
2021-03-16 20:07:03 -0300

    Arquivos do estudo terraform Brenno + Nelson

---
+++ variables.tf
@@ -0,0 +1,54 @@
+variable "region" {
+	default = "us-east-1"
+}
+variable "dnsSupport" {
+	default = "true"
+}
+variable "dnsHostNames" {
+	default = "true"
+}
+variable "instaceTenancy" {
+	default = "default"
+}
+variable "availabilityZone" {
+  type    = list(string)
+  default = ["us-east-1a","us-east-1b","us-east-1cus-east-1dus-east-1e","us-east-1f"]
+}
+variable "VPC_A_vpcCIDRblock" {
+    default = "10.10.0.0/16"
+}
+variable "VPC_A_publicsCIDRblock" {
+    default = "10.10.0.0/24"
+}
+variable "VPC_A_privatesCIDRblock" {
+    default = "10.10.1.0/24"
+}
+variable "VPC_A_dmzCIDRblock" {
+    default = "10.10.2.0/24"
+}
+variable "VPC_A_defaultGTWpublics" {
+    default = "10.10.0.1/24"
+}
+variable "VPC_A_defaultGTWprivates" {
+    default = "10.10.1.1/24"
+}
+variable "VPC_A_defaultGTWdmz" {
+    default = "10.10.2.1/24"
+}
+variable "publicdestCIDRblock" {
+    default = "0.0.0.0/0"
+}
+#variable "localdestCIDRblock" {
+#    default = "10.0.0.0/16"
+#}
+variable "ingressCIDRblock" {
+    type = list
+    default = [ "0.0.0.0/0" ]
+}
+variable "egressCIDRblock" {
+    type = list
+    default = [ "0.0.0.0/0" ]
+}
+#variable "mapPublicIP" {
+#    default = true
+#}
---
+++ vpc.tf
@@ -0,0 +1,111 @@
+#VPC_A
+
+resource "aws_vpc" "VPC_A" {
+  cidr_block           = var.VPC_A_vpcCIDRblock
+  instance_tenancy     = var.instanceTenancy 
+  enable_dns_support   = var.dnsSupport 
+  enable_dns_hostnames = var.dnsHostNames
+tags = {
+    Name = "VPC_A"
+}
+} 
+#######################################################
+resource "aws_subnet" "subnet-pb" {
+  vpc_id                  = aws_vpc.VPC_A.id
+  cidr_block              = var.VPC_A_publicsCIDRblock
+#  map_public_ip_on_launch = var.mapPublicIP 
+  availability_zone       = var.availabilityZone
+tags = {
+   Name = "Public subnet"
+}
+}
+resource "aws_subnet" "subnet-pv" {
+  vpc_id                  = aws_vpc.VPC_A.id
+  cidr_block              = var.VPC_A_privatesCIDRblock
+#  map_public_ip_on_launch = var.mapPublicIP 
+  availability_zone       = var.availabilityZone
+tags = {
+   Name = "Private subnet"
+}
+}
+resource "aws_subnet" "subnet-db" {
+  vpc_id                  = aws_vpc.VPC_A.id
+  cidr_block              = var.VPC_A_dmzCIDRblock
+#  map_public_ip_on_launch = var.mapPublicIP 
+  availability_zone       = var.availabilityZone
+tags = {
+   Name = "DMZ subnet"
+}
+}
+#######################################################
+resource "aws_route_table" "route_public-pb"{
+	vpc_id = aws_vpc.VPC_A.id
+	
+	route {
+		destination_cidr_block = var.VPC_A_publicsCIDRblock
+		gateway_id = var.VPC_A_defaultGTWpublics
+	}
+}
+resource "aws_route_table" "route_public-pv"{
+	vpc_id = aws_vpc.VPC_A.id
+	
+	route {
+		destination_cidr_block = var.VPC_A_privatesCIDRblock
+		gateway_id = var.VPC_A_defaultGTWprivates
+	}
+}
+#######################################################
+resource "aws_route_table_association" "Public_association" {
+  subnet_id      = aws_subnet.subnet-pb.id
+  route_table_id = aws_route_table.route_public-pb.id
+}
+
+
+resource "aws_route_table_association" "Private_association" {
+  subnet_id      = aws_subnet.subnet-pv.id
+  route_table_id = aws_route_table.route_public.id
+}
+#######################################################
+
+
+#VPC_B
+
+
+
+
+
+
+#######################################################
+
+
+#VPC_A-to_VPC_B
+
+resource "aws_vpc_peering_connection" "VPC_A-to_VPC_B" {
+  peer_owner_id = aws_vpc.VPC_A.id
+  peer_vpc_id   = aws_vpc.VPC_B.id
+  vpc_id        = aws_vpc.VPC_A-to_VPC_B.id
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
