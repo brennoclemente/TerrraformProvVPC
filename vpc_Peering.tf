ebb915e6b3a9fecd618c7787d255373b33beb0a8
brennoclemente <brennoclemente@icloud.com>
2021-03-17 17:08:46 -0300

    Update vpc_Peering.tf

--- vpc_Peering.tf
+++ vpc_Peering.tf
@@ -7,6 +7,7 @@ resource "aws_vpc_peering_connection" "VPC_A_to_B" {
   #peer_owner_id = var.IDConta
   peer_vpc_id   = aws_vpc.VPC_B.id
   vpc_id        = aws_vpc.VPC_A.id
+  auto_accept = true
 
   accepter {
     allow_remote_vpc_dns_resolution = true
@@ -30,6 +31,7 @@ resource "aws_vpc_peering_connection" "VPC_A_to_C" {
   #peer_owner_id = var.IDConta
   peer_vpc_id   = aws_vpc.VPC_C.id
   vpc_id        = aws_vpc.VPC_A.id
+  auto_accept = true
 
   accepter {
     allow_remote_vpc_dns_resolution = true
