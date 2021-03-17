174b893434a2ec3d4d6ded036c376c841067e160
brennoclemente <brennoclemente@icloud.com>
2021-03-17 15:57:06 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,7 +14,7 @@ variable "instanceTenancy" {
 	default = "default"
 }
 variable "availabilityZone" {
-  type    = string
+  type    = list
   default = ["us-east-1a","us-east-1b","us-east-1cus-east-1dus-east-1e","us-east-1f"]
 }
 variable "chaveacesso" {
