024faa32f6d200d1e383f0879763c680d518197a
brennoclemente <brennoclemente@icloud.com>
2021-03-17 16:12:07 -0300

    Update vpc_Peering.tf

--- vpc_Peering.tf
+++ vpc_Peering.tf
@@ -6,7 +6,6 @@
 resource "aws_vpc_peering_connection" "VPC_A" {
   peer_owner_id = aws_vpc.VPC_A.id
   peer_vpc_id   = aws_vpc.VPC_B.id
-  peer_vpc_id	= aws_vpc.VPC_C.id
   vpc_id        = aws_vpc.VPC_A.id
 
   accepter {
@@ -23,16 +22,16 @@ resource "aws_vpc_peering_connection" "VPC_A" {
 #######################################################
 
 
-#resource "aws_vpc_peering_connection" "VPC_C" {
-#  peer_owner_id = aws_vpc.VPC_C.id
-#  peer_vpc_id   = aws_vpc.VPC_A.id
-#  vpc_id        = aws_vpc.VPC_C.id
+resource "aws_vpc_peering_connection" "VPC_A" {
+  peer_owner_id = aws_vpc.VPC_A.id
+  peer_vpc_id   = aws_vpc.VPC_C.id
+  vpc_id        = aws_vpc.VPC_A.id
 
-#  accepter {
-#    allow_remote_vpc_dns_resolution = true
-#  }
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
 
-#  requester {
-#    allow_remote_vpc_dns_resolution = true
-#  }
-#}
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+}
