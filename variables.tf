d55126b1eb17f2941239c457618f1fb05a55c89a
brennoclemente <brennoclemente@icloud.com>
2021-03-17 15:24:42 -0300

    Criação dos VPCs B e C que estavam pendentes.

--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
+####################################################################################
+#VARIAVEIS GLOBAIS PARA TODOS OS VPCs AWS
+####################################################################################
 variable "region" {
 	default = "us-east-1"
 }
@@ -14,6 +17,19 @@ variable "availabilityZone" {
   type    = list(string)
   default = ["us-east-1a","us-east-1b","us-east-1cus-east-1dus-east-1e","us-east-1f"]
 }
+variable "chaveacesso" {
+	description = "Digitar a access key"
+	type = string
+}
+variable "chavesecreta" {
+	description = "Digitar a secret key"
+	type = string
+}
+
+####################################################################################
+#Variaveis para o VPC A
+####################################################################################
+
 variable "VPC_A_vpcCIDRblock" {
     default = "10.10.0.0/16"
 }
@@ -35,6 +51,65 @@ variable "VPC_A_defaultGTWprivates" {
 variable "VPC_A_defaultGTWdmz" {
     default = "10.10.2.1/24"
 }
+
+####################################################################################
+#Variaveis para o VPC B
+####################################################################################
+
+variable "VPC_B_vpcCIDRblock" {
+    default = "10.20.0.0/16"
+}
+variable "VPC_B_publicsCIDRblock" {
+    default = "10.20.0.0/24"
+}
+variable "VPC_B_privatesCIDRblock" {
+    default = "10.20.1.0/24"
+}
+variable "VPC_B_dmzCIDRblock" {
+    default = "10.20.2.0/24"
+}
+variable "VPC_B_defaultGTWpublics" {
+    default = "10.20.0.1/24"
+}
+variable "VPC_B_defaultGTWprivates" {
+    default = "10.20.1.1/24"
+}
+variable "VPC_B_defaultGTWdmz" {
+    default = "10.20.2.1/24"
+}
+
+
+####################################################################################
+#Variaveis para o VPC C
+####################################################################################
+
+variable "VPC_C_vpcCIDRblock" {
+    default = "10.30.0.0/16"
+}
+variable "VPC_C_publicsCIDRblock" {
+    default = "10.30.0.0/24"
+}
+variable "VPC_C_privatesCIDRblock" {
+    default = "10.30.1.0/24"
+}
+variable "VPC_C_dmzCIDRblock" {
+    default = "10.30.2.0/24"
+}
+variable "VPC_C_defaultGTWpublics" {
+    default = "10.30.0.1/24"
+}
+variable "VPC_C_defaultGTWprivates" {
+    default = "10.30.1.1/24"
+}
+variable "VPC_C_defaultGTWdmz" {
+    default = "10.30.2.1/24"
+}
+
+
+####################################################################################
+#VARIAVEIS PARA ROTEAMENTO
+####################################################################################
+
 variable "publicdestCIDRblock" {
     default = "0.0.0.0/0"
 }
@@ -60,14 +135,5 @@ variable "egressCIDRblock" {
 
 
 
-#Variaveis de gestão
 
-variable "chaveacesso" {
-	description = "Digitar a access key"
-	type = string
-}
 
-variable "chavesecreta" {
-	description = "Digitar a secret key"
-	type = string
-}
--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
-#VPC_A
-
+###############################################################
+#PROVISIONAMENTO: VPC_A
+###############################################################
 resource "aws_vpc" "VPC_A" {
   cidr_block           = var.VPC_A_vpcCIDRblock
   instance_tenancy     = var.instanceTenancy 
@@ -9,36 +10,46 @@ tags = {
     Name = "VPC_A"
 }
 } 
-#######################################################
-resource "aws_subnet" "subnet-pb" {
+##############################################################
+#Criação Subrede Pública no VPC_A
+##############################################################
+resource "aws_subnet" "VPC_A_subnet-pb" {
   vpc_id                  = aws_vpc.VPC_A.id
   cidr_block              = var.VPC_A_publicsCIDRblock
 #  map_public_ip_on_launch = var.mapPublicIP 
   availability_zone       = var.availabilityZone
 tags = {
-   Name = "Public subnet"
+   Name = "Public subnet VPC A"
 }
 }
-resource "aws_subnet" "subnet-pv" {
+##############################################################
+#Criação Subrede Privada no VPC_A
+##############################################################
+resource "aws_subnet" "VPC_A_subnet-pv" {
   vpc_id                  = aws_vpc.VPC_A.id
   cidr_block              = var.VPC_A_privatesCIDRblock
 #  map_public_ip_on_launch = var.mapPublicIP 
   availability_zone       = var.availabilityZone
 tags = {
-   Name = "Private subnet"
+   Name = "Private subnet VPC A"
 }
 }
-resource "aws_subnet" "subnet-db" {
+##############################################################
+#Criação Subrede DMZ no VPC_A
+##############################################################
+resource "aws_subnet" "VPC_A_subnet-db" {
   vpc_id                  = aws_vpc.VPC_A.id
   cidr_block              = var.VPC_A_dmzCIDRblock
 #  map_public_ip_on_launch = var.mapPublicIP 
   availability_zone       = var.availabilityZone
 tags = {
-   Name = "DMZ subnet"
+   Name = "DMZ subnet VPC A"
 }
 }
-#######################################################
-resource "aws_route_table" "route_public-pb"{
+##############################################################
+#CRIAÇÃO DA TABELA DE ROTEAMENTO DAS SUBREDES PUBLICAS E PRIVADAS
+##############################################################
+resource "aws_route_table" "route_VPC_A_public-pb"{
 	vpc_id = aws_vpc.VPC_A.id
 	
 	route {
@@ -46,7 +57,7 @@ resource "aws_route_table" "route_public-pb"{
 		gateway_id = var.VPC_A_defaultGTWpublics
 	}
 }
-resource "aws_route_table" "route_public-pv"{
+resource "aws_route_table" "route_VPC_A_public-pv"{
 	vpc_id = aws_vpc.VPC_A.id
 	
 	route {
@@ -54,36 +65,196 @@ resource "aws_route_table" "route_public-pv"{
 		gateway_id = var.VPC_A_defaultGTWprivates
 	}
 }
-#######################################################
+##############################################################
+#ASSOCIAÇÃO DAS TABELAS DE ROTEAMENTO AS SUBREDES
+##############################################################
 resource "aws_route_table_association" "Public_association" {
-  subnet_id      = aws_subnet.subnet-pb.id
-  route_table_id = aws_route_table.route_public-pb.id
+  subnet_id      = aws_subnet.VPC_A_subnet-pb.id
+  route_table_id = aws_route_table.route_VPC_A_public-pb.id
 }
 
 
 resource "aws_route_table_association" "Private_association" {
-  subnet_id      = aws_subnet.subnet-pv.id
+  subnet_id      = aws_subnet.VPC_A_subnet-pv.id
   route_table_id = aws_route_table.route_public.id
 }
+###############################################################
+#FIM DO PROVISIONAMENTO: VPC_A
+###############################################################
+
+
+####################################################################################################################
+
+
+###############################################################
+#PROVISIONAMENTO: VPC_B
+###############################################################
+resource "aws_vpc" "VPC_B" {
+  cidr_block           = var.VPC_B_vpcCIDRblock
+  instance_tenancy     = var.instanceTenancy 
+  enable_dns_support   = var.dnsSupport 
+  enable_dns_hostnames = var.dnsHostNames
+tags = {
+    Name = "VPC_B"
+}
+} 
+#################################################################
+#Criação Subrede Pública no VPC_B
+#################################################################
+resource "aws_subnet" "VPC_B_subnet-pb" {
+  vpc_id                  = aws_vpc.VPC_B.id
+  cidr_block              = var.VPC_B_publicsCIDRblock
+#  map_public_ip_on_launch = var.mapPublicIP 
+  availability_zone       = var.availabilityZone
+tags = {
+   Name = "Public subnet VPC B"
+}
+}
+#################################################################
+#Criação Subrede Privada no VPC_B
+#################################################################
+resource "aws_subnet" "VPC_B_subnet-pv" {
+  vpc_id                  = aws_vpc.VPC_B.id
+  cidr_block              = var.VPC_B_privatesCIDRblock
+#  map_public_ip_on_launch = var.mapPublicIP 
+  availability_zone       = var.availabilityZone
+tags = {
+   Name = "Private subnet VPC B"
+}
+}
+#################################################################
+#Criação Subrede DMZ no VPC_B
+#################################################################
+resource "aws_subnet" "VPC_B_subnet-db" {
+  vpc_id                  = aws_vpc.VPC_B.id
+  cidr_block              = var.VPC_B_dmzCIDRblock
+#  map_public_ip_on_launch = var.mapPublicIP 
+  availability_zone       = var.availabilityZone
+tags = {
+   Name = "DMZ subnet VPC B"
+}
+}
+#################################################################
+#CRIAÇÃO DA TABELA DE ROTEAMENTO DAS SUBREDES PUBLICAS E PRIVADAS
+#################################################################
+resource "aws_route_table" "route_VPC_B_public-pb"{
+	vpc_id = aws_vpc.VPC_B.id
+	
+	route {
+		destination_cidr_block = var.VPC_B_publicsCIDRblock
+		gateway_id = var.VPC_B_defaultGTWpublics
+	}
+}
+resource "aws_route_table" "route_VPC_B_public-pv"{
+	vpc_id = aws_vpc.VPC_B.id
+	
+	route {
+		destination_cidr_block = var.VPC_B_privatesCIDRblock
+		gateway_id = var.VPC_B_defaultGTWprivates
+	}
+}
 #######################################################
+#ASSOCIAÇÃO DAS TABELAS DE ROTEAMENTO AS SUBREDES
+resource "aws_route_table_association" "Public_association" {
+  subnet_id      = aws_subnet.VPC_B_subnet-pb.id
+  route_table_id = aws_route_table.route_VPC_B_public-pb.id
+}
 
 
-#VPC_B
+resource "aws_route_table_association" "Private_association" {
+  subnet_id      = aws_subnet.VPC_B_subnet-pv.id
+  route_table_id = aws_route_table.route_public.id
+}
+###############################################################
+#FIM DO PROVISIONAMENTO: VPC_B
+###############################################################
 
 
+####################################################################################################################
 
 
+###############################################################
+#PROVISIONAMENTO: VPC_C
+###############################################################
+resource "aws_vpc" "VPC_C" {
+  cidr_block           = var.VPC_C_vpcCIDRblock
+  instance_tenancy     = var.instanceTenancy 
+  enable_dns_support   = var.dnsSupport 
+  enable_dns_hostnames = var.dnsHostNames
+tags = {
+    Name = "VPC_C"
+}
+} 
+#################################################################
+#Criação Subrede Pública no VPC_C
+#################################################################
+resource "aws_subnet" "VPC_C_subnet-pb" {
+  vpc_id                  = aws_vpc.VPC_C.id
+  cidr_block              = var.VPC_C_publicsCIDRblock
+#  map_public_ip_on_launch = var.mapPublicIP 
+  availability_zone       = var.availabilityZone
+tags = {
+   Name = "Public subnet VPC C"
+}
+}
+#################################################################
+#Criação Subrede Privada no VPC_C
+#################################################################
+resource "aws_subnet" "VPC_C_subnet-pv" {
+  vpc_id                  = aws_vpc.VPC_C.id
+  cidr_block              = var.VPC_C_privatesCIDRblock
+#  map_public_ip_on_launch = var.mapPublicIP 
+  availability_zone       = var.availabilityZone
+tags = {
+   Name = "Private subnet VPC C"
+}
+}
+#################################################################
+#Criação Subrede DMZ no VPC_C
+#################################################################
+resource "aws_subnet" "VPC_C_subnet-db" {
+  vpc_id                  = aws_vpc.VPC_C.id
+  cidr_block              = var.VPC_C_dmzCIDRblock
+#  map_public_ip_on_launch = var.mapPublicIP 
+  availability_zone       = var.availabilityZone
+tags = {
+   Name = "DMZ subnet VPC C"
+}
+}
+#################################################################
+#CRIAÇÃO DA TABELA DE ROTEAMENTO DAS SUBREDES PUBLICAS E PRIVADAS
+#################################################################
+resource "aws_route_table" "route_VPC_C_public-pb"{
+	vpc_id = aws_vpc.VPC_C.id
+	
+	route {
+		destination_cidr_block = var.VPC_C_publicsCIDRblock
+		gateway_id = var.VPC_C_defaultGTWpublics
+	}
+}
+resource "aws_route_table" "route_VPC_C_public-pv"{
+	vpc_id = aws_vpc.VPC_C.id
+	
+	route {
+		destination_cidr_block = var.VPC_C_privatesCIDRblock
+		gateway_id = var.VPC_C_defaultGTWprivates
+	}
+}
+#######################################################
+#ASSOCIAÇÃO DAS TABELAS DE ROTEAMENTO AS SUBREDES
+resource "aws_route_table_association" "Public_association" {
+  subnet_id      = aws_subnet.VPC_C_subnet-pb.id
+  route_table_id = aws_route_table.route_VPC_C_public-pb.id
+}
 
 
-
-
-
-
-
-
-
-
-
+resource "aws_route_table_association" "Private_association" {
+  subnet_id      = aws_subnet.VPC_C_subnet-pv.id
+  route_table_id = aws_route_table.route_public.id
+}
+###############################################################
+#FIM DO PROVISIONAMENTO: VPC_C
+###############################################################
 
 
 
