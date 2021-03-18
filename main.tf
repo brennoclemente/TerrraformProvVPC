298e3f294e127fa51542960820d718f6424a48be
brennoclemente <brennoclemente@icloud.com>
2021-03-18 10:00:20 -0300

    Atualização do provisionamento without ID ACCOUNT
    Atualizando o provisionamento da AWS sem necessidade do ID ACCOUNT pro VPC peering

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
+################################
+#CRIAÇÃO DO PROVISIONAMENTO AWS
+################################
+
+
 provider "aws" {
 	region = "us-east-1"
 	access_key = "${var.chaveacesso}" 
 	secret_key = "${var.chavesecreta}"
 }
-#ATUALIZADO
+
